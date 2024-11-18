908daa4db492b216ce8c9264d5e6141e9b45d88b
Sunghyeon <tjdgustjdan@gmail.com>
2024-11-18 10:50:19 +0900

    feat: repository pattern 적용

---
+++ app/src/main/java/com/example/boo345word/data/repository/WordRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.example.boo345word.data.repository
+
+import com.example.boo345word.data.entity.BasicWord
+import com.example.boo345word.data.source.WordDataSource
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class WordRepositoryImpl
+    @Inject
+    constructor(
+        private val dataSource: WordDataSource,
+    ) : WordRepository {
+        override suspend fun getFiveBasicWords(): Flow<List<BasicWord>> = dataSource.getFiveBasicWords()
+
+        override suspend fun markWordAsLearned(word: String) = dataSource.markWordAsLearned(word)
+    }
